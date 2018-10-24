12acd523c2d294e41065c94cefb9c637362c1e57
Hoseung Lee <astrohsy@gmail.com>
2018-10-24 15:38:12 +0900

    Implement Graph Class and Cycle Detection

---
+++ server/src/utils/graph/graph.spec.ts
@@ -0,0 +1,66 @@
+import 'jest';
+
+import { Graph } from './graph';
+
+describe('Graph Util', () => {
+
+  describe('isCycle', () => {
+    it('should return false without Cycle', async () => {
+      const g = new Graph();
+      
+      g.addEdge(1, 2);
+      g.addEdge(1, 3);
+      g.addEdge(1, 4);
+      g.addEdge(1, 5);
+
+      g.addEdge(5, 3);
+      g.addEdge(5, 4);
+      g.addEdge(5, 6);
+      g.addEdge(5, 7);
+      g.addEdge(5, 8);
+
+      expect(g.isCycle()).toEqual(false);
+    });
+
+    it('should return true with Cycle', async () => {
+      const g = new Graph();
+      
+      g.addEdge(1, 2);
+      g.addEdge(1, 3);
+      g.addEdge(1, 4);
+      g.addEdge(1, 5);
+
+      g.addEdge(5, 6);
+      g.addEdge(5, 7);
+      g.addEdge(5, 8);
+      g.addEdge(5, 9);
+      g.addEdge(5, 10);
+
+      // Cycle
+      g.addEdge(5, 1)
+
+      expect(g.isCycle()).toEqual(true);
+    });
+
+
+    it('should return true with transitive Cycle', async () => {
+      const g = new Graph();
+      
+      g.addEdge(1, 2);
+      g.addEdge(1, 3);
+      g.addEdge(1, 4);
+      g.addEdge(1, 5);
+
+      g.addEdge(5, 6);
+      g.addEdge(5, 7);
+      g.addEdge(5, 8);
+      g.addEdge(5, 9);
+      g.addEdge(5, 10);
+
+      // Cycle
+      g.addEdge(10, 1)
+
+      expect(g.isCycle()).toEqual(true);
+    });
+  });
+})
